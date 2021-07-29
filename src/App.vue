438f43729a8de772eabbfc40e6be74c7882db981
yegorich75 <yegorich75@gmail.com>
2021-07-29 22:44:54 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+        <!-- header -->
+        <div class="wrapper-content">
+            <div class="view">
+                <div class="container">
+                    <List />
+                </div>
+            </div>
+        </div>
+        <!-- footer -->
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from "@/components/List";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    components: { List },
+};
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/List.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul>
+        <li
+            v-for="(tweet, index) in items"
+            :key="tweet.id">
+            <img :src="tweet.avatar" width="100" height="100">
+            <span>{{tweet.date}}</span>
+            <div>{{tweet.body}}</div>
+        </li>
+    </ul>
+</template>
+
+<script>
+
+
+export default {
+    data() {
+        return {
+            items: [
+                {
+                    id: 1,
+                    body: 'hello vue 3',
+                    likes: 12,
+                    avatar: `https://avatars.dicebear.com/api/male/1.svg`,
+                    date: new Date(Date.now()).toLocaleString(),
+                },
+                {
+                    id: 2,
+                    body: 'hello world',
+                    likes: 6,
+                    avatar: `https://avatars.dicebear.com/api/male/2.svg`,
+                    date: new Date(Date.now()).toLocaleString(),
+                }
+            ],
+        }
+    },
+}
+</script>
