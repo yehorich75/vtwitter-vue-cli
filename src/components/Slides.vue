85a8f7fa8e271b7d10d6dd1f46ed881dafb17f9e
yegorich75 <yegorich75@gmail.com>
2021-08-12 06:46:41 +0300

    first commit

--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -1,81 +1,28 @@
 <template>
-    <div class="slides">
-        <div class="container">
-            <div class="slides-inner">
-                <div :class="slideClass" v-for="slide in slides" :key="slide.id">
-                    <p>{{ singleWidth }}</p>
-                    <br />
-                    <p>{{ innerWidth }}</p>
-                    <img :class="slideImgClass" :src="slide.src">
-                    <!-- <Slide :slide="slide" /> -->
-                </div>
-            </div>
-        </div>
+    <carousel :items-to-show="1.5">
+        <slide v-for="slide in 3" :key="slide">
+            {{ slide }}
+        </slide>
 
-    </div>
+        <template #addons>
+            <navigation />
+            <pagination />
+        </template>
+    </carousel>
 </template>
 
 <script>
-import Slide from './Slide.vue';
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel';
+
 export default {
-    // setup() {
-    //     onMounted() {
-    //         this.$nextTick(() => {
-    //             let singleWidth = this.$el.clientWidth/this.itemsPerSlide;
-    //             this.$set(this.$data, 'singleWidth', singleWidth);
-    //             this.$set(this.$data, 'innerWidth', singleWidth * this.slides.length);
-    //         })
-    //     }
-    // },
-    data () {
-        return {
-            slides: [
-                { src: require('../assets/images/slider/slider-colorful-gifts.jpg'), id: 0 },
-                { src: require('../assets/images/slider/slider-detailed-elegance.jpg'), id: 1 },
-                { src: require('../assets/images/slider/slider-perfect-chandeliers.jpg'), id: 2 }
-            ],
-            slideClass: 'slide',
-            slideImgClass: 'slide__img',
-            innerWidth: 0,
-            singleWidth: 0
-        }
-    },
-    props: {
-        slide: ['slide'],
-        itemsPerSlide: {
-            type: null,
-            default: 1
-        }
-    },
+    name: 'Slides',
     components: {
-        Slide
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
     },
-    mounted() {
-        this.$nextTick(() => {
-            let singleWidth = this.$el.clientWidth/this.itemsPerSlide;
-            this.$set(this.$data, 'singleWidth', singleWidth);
-            this.$set(this.$data, 'innerWidth', singleWidth * this.slides.length);
-        });
-    }
-}
+};
 </script>
-
-<style lang="scss" scoped>
-
-.slides {
-    &-inner {
-        display: flex;
-        // width: 4500px;
-        // overflow: hidden;
-    }
-}
-
-.slide {
-    width: 700px;
-    img {
-        max-width: 700px;
-        height: auto;
-    }
-}
-
-</style>style
